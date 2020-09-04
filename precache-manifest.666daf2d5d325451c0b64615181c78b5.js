55c4383c2534d2fc5f1d1e5190862196e9fadacb
mnvssvamsi <mnvssvamsi@users.noreply.github.com>
2020-09-04 16:37:15 +0530

    Updates

---
+++ precache-manifest.666daf2d5d325451c0b64615181c78b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680ee00fd3e0b7a7d3c5bccee476422f",
+    "url": "/Chamb-Website/index.html"
+  },
+  {
+    "revision": "796ef2ca98bccf3e8079",
+    "url": "/Chamb-Website/static/css/main.7ea35d2e.chunk.css"
+  },
+  {
+    "revision": "6c56a5e9d5fd5508f411",
+    "url": "/Chamb-Website/static/js/2.b4e59ae2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chamb-Website/static/js/2.b4e59ae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796ef2ca98bccf3e8079",
+    "url": "/Chamb-Website/static/js/main.9b1697cf.chunk.js"
+  },
+  {
+    "revision": "dc4402dc30a648d45e06",
+    "url": "/Chamb-Website/static/js/runtime-main.8a666b20.js"
+  },
+  {
+    "revision": "ec13b457a74f1d615db496c8e8a6c29c",
+    "url": "/Chamb-Website/static/media/bg_main.ec13b457.png"
+  },
+  {
+    "revision": "f87cdbd7ad1526417a25ba45306e4fd5",
+    "url": "/Chamb-Website/static/media/footer_bg.f87cdbd7.png"
+  },
+  {
+    "revision": "312d0c8103982ca5d8bb1ce3327715d7",
+    "url": "/Chamb-Website/static/media/works-icon-01.312d0c81.png"
+  },
+  {
+    "revision": "867260194c6dd535f91675b012df44e6",
+    "url": "/Chamb-Website/static/media/works-icon-02.86726019.png"
+  },
+  {
+    "revision": "c7bf275813fc6e759f7bb89db0e1ec43",
+    "url": "/Chamb-Website/static/media/works-icon-03.c7bf2758.png"
+  }
+]);
